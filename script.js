ce38709dad0aa01922f3e0c9c7e60541ab9d0fa1
Sterva <gaberkornvadim8@gmail.com>
2024-02-03 21:16:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const obj = {
+    name: 'asd',
+    age:20,
+    surename: undefined
+}
+
+const arr  = ['name','age']
+
+//достуа к обьекту через квадратные скобки и ключ,возвращает значение этого ключа
+obj[]
